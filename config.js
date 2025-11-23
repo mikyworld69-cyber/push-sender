930836a586fb2b07b6650f8b4b951697cd986ef0
mikyworld69-cyber <mikyworld69@gmail.com>
2025-11-23 21:15:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ export default {
   sharedSecret: "43534534gdggr5646487867gfghff",
 
   vapid: {
-    subject: "mailto:info@iappsweb.com",
-    publicKey: "BKeTNaLfcsCBf0MqBYpUdbGv5dN0kdkHu_rfrdqGLmKPsa1nH1SUfSsNiA_IUbWjvYFIoBSTVky6mNdTCOFFw_k",
-    privateKey: "5X8PCAHdcwrWl-Io926rsE7oZ7Bcbuka9AJW9WuHorw"
+  subject: "mailto:info@iappsweb.com",
+  publicKey: "BKeTNaLfcsCBf0MqBYpUdbGv5dN0kdkHu_rfrdqGLmKPsa1nH1SUfSsNiA_IUbWjvYFIoBSTVky6mNdTCOFFw_k",
+  privateKey: "5X8PCAHdcwrWl-Io926rsE7oZ7BcbzQy3RietAHYfjJg_OHxUxELuo-8NQC5dlWDORKUeNwXo6LL0"
   }
 };
