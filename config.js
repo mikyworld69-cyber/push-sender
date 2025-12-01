01bd9a29a72472bdd5ca4e0c07f37d32de11a7df
mikyworld69-cyber <mikyworld69@gmail.com>
2025-12-01 08:56:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ export default {
 
   vapid: {
   subject: "mailto:info@iappsweb.com",
-  $VAPID_PUBLIC  = "BMiG3M8_AjexqixVrT6ChNFYQ5yUysgKzj8OQaV7NlqbsbpbfmPQlyPPu4seY30mlMxz51k7KtEDlmFxGvOjx_k";
-  $VAPID_PRIVATE = "YDWYqkEBt4dI4A9Z3yWbnWZIgYJoTUBgT8lJzjWAjXA";
+  $VAPID_PUBLIC  = "BF9_KKtwjRzBqjH9hOeYdHqQcw9gMwN2J3c-nFvR8Lcl-uWlyY2gUOjUz0OnXjGKqnqW9NPUZ_7y9MGiYFtS1hA";
+  $VAPID_PRIVATE = "fPjPuvNTpTJS7fE4yF0JMoS61u13xPXuVmy27ZUW2mI";
 
   }
 };
